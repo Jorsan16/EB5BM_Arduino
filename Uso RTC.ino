2821e9f8f5b0a883a9a1535147aad0e8c2aadc1e
Jorge Sánchez <105458901+Jorsan16@users.noreply.github.com>
2023-05-03 11:32:30 -0600

    Uso de RTC
    El siguiente codigo se muestra el uso del modulo de RTC (Real Time Clock)

---
+++ Uso RTC.ino
@@ -0,0 +1,31 @@
+
+
+#include <Wire.h>		// incluye libreria para interfaz I2C
+#include <RTClib.h>		// incluye libreria para el manejo del modulo RTC
+
+RTC_DS3231 rtc;			// crea objeto del tipo RTC_DS3231
+
+void setup () {
+ Serial.begin(9600);		// inicializa comunicacion serie a 9600 bps
+  rtc.begin();	//Inicializa el modulo RTC
+
+ rtc.adjust(DateTime(__DATE__, __TIME__));	// funcion que permite establecer fecha y horario
+
+
+void loop () {
+ DateTime fecha = rtc.now();			// funcion que devuelve fecha y horario en formato
+														    	// DateTime y asigna a variable fecha
+ Serial.print(fecha.day());			// funcion que obtiene el dia de la fecha completa
+ Serial.print("/");				// caracter barra como separador
+ Serial.print(fecha.month());			// funcion que obtiene el mes de la fecha completa
+ Serial.print("/");				// caracter barra como separador
+ Serial.print(fecha.year());			// funcion que obtiene el año de la fecha completa
+ Serial.print(" ");				// caracter espacio en blanco como separador
+ Serial.print(fecha.hour());			// funcion que obtiene la hora de la fecha completa
+ Serial.print(":");				// caracter dos puntos como separador
+ Serial.print(fecha.minute());			// funcion que obtiene los minutos de la fecha completa
+ Serial.print(":");				// caracter dos puntos como separador
+ Serial.println(fecha.second());		// funcion que obtiene los segundos de la fecha completa
+ 
+ delay(1000);			
+}
