dde1a60aa00f0ecbe9f3356bb0c24dc2ff54e0fc
Jorge Sánchez <105458901+Jorsan16@users.noreply.github.com>
2023-07-09 15:29:12 -0600

    Update Uso_Sensor_Voltaje.ino

--- Bimestre_3/Uso_Sensor_Voltaje.ino
+++ Bimestre_3/Uso_Sensor_Voltaje.ino
@@ -6,7 +6,7 @@ Serial.begin(9600);
 
 void loop() {
   
-float voltaje =  (float)25*analogRead(A0)/1023;
+float voltaje =  25*analogRead(A0)/1023;
 Serial.print("Voltaje medido =  ");
 Serial.println(voltaje);
 delay(500);
