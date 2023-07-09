1f10b7890985e76304f56e314a3f3503d8d43f83
Jorge Sánchez <105458901+Jorsan16@users.noreply.github.com>
2023-07-09 15:21:46 -0600

    Create Uso_Sensor_Corriente.ino

---
+++ Bimestre_3/Uso_Sensor_Corriente.ino
@@ -0,0 +1,15 @@
+float Sensibilidad=0.185; //sensibilidad en Voltios/Amperio para sensor de 5A
+
+void setup() {
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  float voltajeSensor= analogRead(A0)*(5.0 / 1023.0); //lectura del sensor   
+  float I=(voltajeSensor-2.5)/Sensibilidad; //Ecuación  para obtener la corriente
+  Serial.print("Corriente: ");
+  Serial.println(I,3); 
+  delay(200);     
+}
