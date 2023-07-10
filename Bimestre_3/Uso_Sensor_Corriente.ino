c9dcbe5d02e3f19db966937ebf0412d660ce481f
Jorge Sánchez <105458901+Jorsan16@users.noreply.github.com>
2023-07-10 09:20:51 -0600

    Update Uso_Sensor_Corriente.ino

--- Bimestre_3/Uso_Sensor_Corriente.ino
+++ Bimestre_3/Uso_Sensor_Corriente.ino
@@ -1,15 +1,20 @@
-float Sensibilidad=0.185; //sensibilidad en Voltios/Amperio para sensor de 5A
+const float Factor_1 = 0.185; //Factor de Medicion para lectura de -5A a 5A
+const float Factor_2 = 0.1; //Factor de Medicion para lectura de -20A a 20A
+const float Factor_3 = 0.066; //Factor de Medicion para lectura de -30A a 30A
+int Lectura = 0; // Lectura del pin A0
+double Corriente = 0; //Guarda el valor de la corriente leida en el sensor
+float voltajeSensor = 0; 
 
 void setup() {
-  
   Serial.begin(9600);
 }
 
 void loop() {
-  
-  float voltajeSensor= analogRead(A0)*(5.0 / 1023.0); //lectura del sensor   
-  float I=(voltajeSensor-2.5)/Sensibilidad; //Ecuación  para obtener la corriente
+  Lectura = analogRead(A0);
+  voltajeSensor = (Lectura*5.0) / 1023.0; //lectura del sensor   
+  Corriente = (voltajeSensor - 2.5)/Factor_3; //Ecuación  para obtener la corriente
   Serial.print("Corriente: ");
-  Serial.println(I,3); 
+  Serial.print(I,3); 
+  Serial.println(" A"); 
   delay(200);     
 }
