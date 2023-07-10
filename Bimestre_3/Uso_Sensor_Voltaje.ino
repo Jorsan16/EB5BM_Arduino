64eb45027b08f20133ae1b52d856adb8306bde22
Jorge Sánchez <105458901+Jorsan16@users.noreply.github.com>
2023-07-10 09:03:05 -0600

    Update Uso_Sensor_Voltaje.ino

--- Bimestre_3/Uso_Sensor_Voltaje.ino
+++ Bimestre_3/Uso_Sensor_Voltaje.ino
@@ -1,14 +1,15 @@
-const float Factor = 1.25;
-int lectura = 0;
+const float Factor = 1.25; // Constante del Factor de Lectura
+int lectura = 0; //Guardar La lectura hecha por el sensor
+
 void setup() {
-Serial.begin(9600);
+Serial.begin(9600); // Inicializamos la comunicacion Serial
 }
 
 void loop() {
-lectura = analogRead(A0);
-float voltaje =  (25*lectura*Factor)/1023;
+lectura = analogRead(A0); //Para hacer la lectura del pin A0
+float voltaje =  (25*lectura*Factor)/1023; // Conversion de lectura analogica a valores de medicion
 Serial.print("Voltaje medido =  ");
-Serial.print(voltaje);
+Serial.print(voltaje); // Muestra en el monitor serial la lectura
 Serial.println("V");
 delay(250);
 }
